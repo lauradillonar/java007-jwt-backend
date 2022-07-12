eafa900a4c6c21202abe3ba1b8af815a8034c6bb
lauradillonar <laura.dillon.ar@gmail.com>
2022-07-12 17:44:52 -0300

    Implementación de las entidades del paquete security.entity

--- src/main/java/com/confluenciacreativa/crud/security/entity/Rol.java
+++ src/main/java/com/confluenciacreativa/crud/security/entity/Rol.java
@@ -1,4 +1,65 @@
 package com.confluenciacreativa.crud.security.entity;
 
+import com.confluenciacreativa.crud.security.enums.RolNombre;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public Rol(int id, RolNombre rolNombre) {
+        this.id = id;
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/confluenciacreativa/crud/security/enums/RolNombre.java
+++ src/main/java/com/confluenciacreativa/crud/security/enums/RolNombre.java
@@ -1,4 +1,5 @@
 package com.confluenciacreativa.crud.security.enums;
 
 public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
 }
