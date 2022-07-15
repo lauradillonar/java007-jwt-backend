50e3f5d5bb6bcc3bdc5436e69963e8ca09463cb3
lauradillonar <laura.dillon.ar@gmail.com>
2022-07-15 17:27:13 -0300

    Soluciono inyección de UserDetailsServiceImpl

--- src/main/java/com/confluenciacreativa/crud/security/MainSecurity.java
+++ src/main/java/com/confluenciacreativa/crud/security/MainSecurity.java
@@ -2,6 +2,7 @@ package com.confluenciacreativa.crud.security;
 
 import com.confluenciacreativa.crud.security.jwt.JwtEntryPoint;
 import com.confluenciacreativa.crud.security.jwt.JwtTokenFilter;
+import com.confluenciacreativa.crud.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,7 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class MainSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     JwtEntryPoint jwtEntryPoint;
--- src/main/java/com/confluenciacreativa/crud/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/confluenciacreativa/crud/security/service/UserDetailsServiceImpl.java
@@ -4,11 +4,12 @@ import com.confluenciacreativa.crud.security.entity.Usuario;
 import com.confluenciacreativa.crud.security.entity.UsuarioPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     UsuarioService usuarioService;
